a208f0797a3153cc9628f0c9dfd9ef8fb2b25d25
mkhoa <mkhoa94@gmail.com>
2020-10-06 21:40:55 +0700

    First Commit

---
+++ detection.py
@@ -0,0 +1,100 @@
+import urllib
+import pymysql
+import os
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import cv2
+import math
+import google.auth
+import datetime
+import six
+import requests
+
+
+from google.cloud import vision
+from google.cloud import storage
+from skimage import io
+from six.moves.urllib.parse import quote
+from PIL import Image
+
+#Parameter
+project_id = 'abstract-veld-289612'
+bucket_name = 'ftmle'
+storage_client = storage.Client.from_service_account_json("/home/mkhoa/CoderSchool/FurniturePairing/Credentials/abstract-veld-289612-327ddac80eba.json")
+blob = 'Images/Products/146331-0-home-design.jpg'
+
+def generate_download_signed_url_v4(bucket_name, blob_name):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    """
+    # bucket_name = 'your-bucket-name'
+    # blob_name = 'your-object-name'
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=15),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    # print("Generated GET signed URL:")
+    # print(url)
+    # print("You can use this URL with any user agent, for example:")
+    # print("curl '{}'".format(url))
+    return url
+#print(generate_download_signed_url_v4(bucket_name, blob))
+
+
+def generate_upload_signed_url_v4(bucket_name, blob_name):
+    """Generates a v4 signed URL for uploading a blob using HTTP PUT.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    """
+    # bucket_name = 'your-bucket-name'
+    # blob_name = 'your-object-name'
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=15),
+        # Allow PUT requests using this URL.
+        method="PUT",
+        content_type="application/octet-stream",
+    )
+
+    print("Generated PUT signed URL:")
+    print(url)
+    print("You can use this URL with any user agent, for example:")
+    print(
+        "curl -X PUT -H 'Content-Type: application/octet-stream' "
+        "--upload-file my-file '{}'".format(url)
+    )
+    return url
+
+# test_img = Image.open('test.jpg')
+# print(te)
+img = open('test.jpg', 'rb')
+blob_name = 'Images/Uploads/test.jpg'
+def upload_to_gcs(img, bucket_name, blob_name):
+    upload_url = generate_upload_signed_url_v4(bucket_name, blob_name)
+    headers = {'content-type': 'application/octet-stream'}
+    response = requests.put(upload_url, data=img, headers=headers)
+    return response.status_code
+
+
+    
+
---
+++ streamlit.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import os
+import sys
+import datetime
+
+from pathlib import Path
+from enum import Enum
+from io import BytesIO, StringIO
+from typing import Union
+from detection import upload_to_gcs
+
+
+sys.setrecursionlimit(15000)
+script_location = Path(__file__).absolute().parent
+
+STYLE = """
+<style>
+img {
+    max-width: 100%;
+}
+</style>
+"""
+
+def main():
+    '''Main function that will run the whole app
+    
+    '''
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+    st.title('Pair: A Cross-Category Furniture Recommendation Engine')
+    st.sidebar.title("What to do")
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    app_mode = st.sidebar.selectbox("Choose the app mode",
+        ["Introduction", "Furniture Detection", "Furniture Pairing"])
+    if app_mode == "Introduction":
+        st.sidebar.success('To continue select "Run the app".')
+        intro()
+    elif app_mode == "Furniture Detection":
+        st.write('Furniture Detection')
+    elif app_mode == "Furniture Pairing":
+        st.write('Furniture Detection')
+        
+# Introduction
+def intro():
+    """
+    Upload File on Streamlit Code
+    
+    """
+    fileTypes = ["csv", "png", "jpg"]  
+    st.markdown(STYLE, unsafe_allow_html=True)
+    file = st.file_uploader("Upload file", type=fileTypes)
+    show_file = st.empty()
+    if not file:
+        show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
+        return
+    content = file.getvalue()
+    if isinstance(file, BytesIO):
+        show_file.image(file)
+        try:
+            fname = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
+            bucket_name = 'ftmle'
+            blob_name = 'Images/Uploads/test.jpg'
+            res = upload_to_gcs(content, bucket_name, blob_name)
+            st.write('Response Code: ' + res)
+            st.write('Show File: ' + )
+        except:
+            st.write('Error')
+    else:
+        data = pd.read_csv(file)
+        st.dataframe(data.head(10))
+        file.close()
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+if __name__ == "__main__":
+    main()
